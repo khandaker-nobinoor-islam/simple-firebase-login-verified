2d3b21c535b23545f7e158752f538cd13ef3d4f7
Your Name <khandakerdipu146@gmail.com>
2023-10-19 14:42:21 +0600

    first login and logout verified

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBIZ5JtkepsD5Dfikd1yC0qlM2baht92wk",
+  authDomain: "simple-firebase-2fa56.firebaseapp.com",
+  projectId: "simple-firebase-2fa56",
+  storageBucket: "simple-firebase-2fa56.appspot.com",
+  messagingSenderId: "500953676951",
+  appId: "1:500953676951:web:c183efb6492160cba8fdd2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/steps.js
@@ -0,0 +1,34 @@
+/*
+*                    -----------------------------
+*                           INITIAL SETUP
+*                    ------------------------------
+* 1. visit: console .firebase.google.com
+* 2. create project (skip google analytics)
+* 3. register app (create config)
+* 4. install firebase: nap install firebase
+* 5. add config file to your project
+* 6. DANGER: Do not public or make firebase config to public by pushing those to github
+*                     ------------------------------
+*                             INTEGRATION
+*                     ------------------------------
+* 7. visits: Go to Docs > Build authentication > web > get started
+* 8. export app from the firebase.config.js file: export default app
+* 9. Login.jsx: import getAuth from firebase/auth
+* 10. create const auth = getAuth(app)
+*                      ------------------------------
+*                             PROVIDER SETUP
+*                      ------------------------------
+* 11. import googleAuthProvider and create a new provider
+* 12. use signInWithPopUp and pass auth and provider
+* 13. activate sign-in method (google, facebook, github, etc.)
+* 14. [vite]: change 12.0.0.1 localhost
+* 14. 
+*                    ------------------------------
+*                           More Auth Provider
+*                    ------------------------------
+* 15. activate the auth provider (create app, provider redirect url, client id, client secret)
+* 16. 
+* 18. 
+* 19. 
+* 20. 
+*/
